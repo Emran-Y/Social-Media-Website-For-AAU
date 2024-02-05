1e23197fb8a677ec9f2bb9670a62c2a8c903adb4
EMRAN YONAS YIMER <emranyonas602@gmail.com>
2024-02-05 17:19:03 +0300

    deploy

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Layout from "./components/layout";
-import Login from "./pages/login/login";
-import Signup from "./pages/signup/signup";
-import Announcement from "./pages/announcement/Announcement";
-import JobsAndInternships from "./pages/JobsAndInternships/JobsAndInternships";
-import LostAndFound from "./pages/lostAndFound/LostAndFound";
-import Profile from "./pages/profile/Profile";
-import UserProfile from "./pages/UserProfile/userProfile";
-import NotFound from "./pages/NotFound/NotFound";
+import Login from "@pages/login/login";
+import Signup from "@pages/signup/signup";
+import Announcement from "@pages/announcement/Announcement";
+import JobsAndInternships from "@pages/JobsAndInternships/JobsAndInternships";
+import LostAndFound from "@pages/lostAndFound/LostAndFound";
+import Profile from "@pages/profile/Profile";
+import UserProfile from "@pages/UserProfile/userProfile";
+import NotFound from "@pages/NotFound/NotFound";
 import MyClubs from "./ConditionalPages/MyClubs";
 import Clubs from "./ConditionalPages/clubs";
-import Home from "./pages/home/home"; // Import your Home component
-import OneAnnouncement from "./pages/oneAnnouncement/OneAnnouncement";
+import Home from "@pages/home/home"; // Import your Home component
+import OneAnnouncement from "@pages/oneAnnouncement/OneAnnouncement";
 
 function App() {
   return (
--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@pages": "/src/pages",
+    },
+  },
+});
+
+// Example vite.config.js
+// import { defineConfig } from 'vite';
+
+// export default defineConfig({
+//   // ... other configurations ...
+//   resolve: {
+//     alias: {
+//       '@pages': '/src/pages',
+//     },
+//   },
+// });
